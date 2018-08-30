a1271d0d717e458e44500bf70fb9fbe1d1c0b530
leuat <nicolaas.groeneboom@gmail.com>
2018-08-30 11:15:26 +0200

    Ready for 0.03 releae

---
+++ Publish/tutorials/TutorialGame_RogueBurgerOne/prepare.sh
@@ -0,0 +1,4 @@
+exomizer2 mem -lnone images/introscreen_data.bin@0xA000 -o images/introscreen_data_compressed.bin 
+exomizer2 mem -lnone  levels/levels.flf@0x9000 -o levels/levels_compressed.bin 
+exomizer2 images/logoscreen_data.bin@0xA000 -o images/logoscreen_data_compressed.bin 
+pause
