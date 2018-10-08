3d4ece2b84c3758ba8d836d410e08c50b460b644
Leuat <leuat@irio.co.uk>
2018-10-08 08:56:55 +0200

    Mac changes

--- Publish/publish_mac/publish_mac.sh
+++ Publish/publish_mac/publish_mac.sh
@@ -6,6 +6,7 @@ cp -r ../tutorials/RasLib trse/RasLib/
 cp -r ../tutorials/Tutorials trse/Tutorials
 cp -r ../tutorials/TutorialGame_RogueBurgerOne trse/TutorialGame_RogueBurgerOne
 cp -r ../tutorials/TutorialGame_Introduction trse/TutorialGame_Introduction
+cp -r ../tutorials/Vic20 trse/Vic20
 cd trse/trse.app
  ~/Qt/5.11.0/clang_64/bin/macdeployqt .
  rm *.ini
@@ -14,7 +15,8 @@ cd trse/trse.app
  find trse/Tutorials/ -name '*.asm' -delete
  find trse/TutorialGame_RogueBurgerOne/ -name '*.asm' -delete
  find trse/TutorialGame_Introduction/ -name '*.asm' -delete
+ find trse/Vic20/ -name '*.asm' -delete
 tar -cvf trse_osx.tar trse/
 gzip trse_osx.tar
-scp trse_osx.tar.gz leuat@www.irio.co.uk:www/trse
+scp trse_osx.tar.gz leuat@www.irio.co.uk:www/trse/latest_snapshots/
 
