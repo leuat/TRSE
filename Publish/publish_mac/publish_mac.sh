7eb3457dae1c4ab30ed7636d6e349110f1e3b44a
Leuat <leuat@irio.co.uk>
2018-10-19 21:47:46 +0200

    Mac update

--- Publish/publish_mac/publish_mac.sh
+++ Publish/publish_mac/publish_mac.sh
@@ -3,19 +3,17 @@ mkdir trse
 cp -r ../../release/trse.app trse/
 cp -r  ../source/* trse/trse.app/	
 cp -r ../tutorials/RasLib trse/RasLib/
-cp -r ../tutorials/Tutorials trse/Tutorials
-cp -r ../tutorials/TutorialGame_RogueBurgerOne trse/TutorialGame_RogueBurgerOne
-cp -r ../tutorials/TutorialGame_Introduction trse/TutorialGame_Introduction
 cp -r ../tutorials/Vic20 trse/Vic20
+cp -r ../tutorials/C64 trse/C64
+cp -r ../tutorials/C128 trse/C128
 cd trse/trse.app
  ~/Qt/5.11.0/clang_64/bin/macdeployqt .
  rm *.ini
  cd ../../
  find trse -name '*.prg' -delete
- find trse/Tutorials/ -name '*.asm' -delete
- find trse/TutorialGame_RogueBurgerOne/ -name '*.asm' -delete
- find trse/TutorialGame_Introduction/ -name '*.asm' -delete
+ find trse/C64/ -name '*.asm' -delete
  find trse/Vic20/ -name '*.asm' -delete
+ find trse/C128/ -name '*.asm' -delete
 tar -cvf trse_osx.tar trse/
 gzip trse_osx.tar
 scp trse_osx.tar.gz leuat@www.irio.co.uk:www/trse/latest_snapshots/
