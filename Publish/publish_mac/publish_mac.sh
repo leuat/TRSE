faf6b6957e88e5262c30269079dd103d9905bf06
Leuat <leuat@irio.co.uk>
2019-05-19 14:20:14 +0200

    Mac update

--- Publish/publish_mac/publish_mac.sh
+++ Publish/publish_mac/publish_mac.sh
@@ -8,6 +8,8 @@ cp -r ../tutorials/C64 trse/C64
 cp -r ../tutorials/C128 trse/C128
 cp -r ../tutorials/NES trse/NES
 cp -r ../tutorials/Fjong trse/Fjong
+cp -r ../tutorials/PET trse/Amiga
+cp -r ../tutorials/Amiga trse/Amiga
 cd trse/trse.app
  ~/Qt/5.11.0/clang_64/bin/macdeployqt .
  rm *.ini
@@ -18,6 +20,8 @@ cd trse/trse.app
  find trse/C128/ -name '*.asm' -delete
  find trse/NES/ -name '*.asm' -delete
  find trse/Fjong/ -name '*.asm' -delete
+ find trse/PET/ -name '*.asm' -delete
+ find trse/Amiga/ -name '*.asm' -delete
 tar -cvf trse_osx.tar trse/
 gzip trse_osx.tar
 scp trse_osx.tar.gz leuat@www.irio.co.uk:www/trse/latest_snapshots/
