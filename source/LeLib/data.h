501f5e16a3d8b0ce30486263c0c588fad0cc89ce
leuat <leuat@irio.co.uk>
2020-05-14 10:49:31 +0200

    Ready for 0.3

--- source/LeLib/data.h
+++ source/LeLib/data.h
@@ -30,7 +30,7 @@ class Data
 public:
     Data();
     int flfVersion = 2;
-    QString version = "0.10.2";
+    QString version = "0.10.3";
 
     bool redrawOutput=false;
     bool redrawInput=false;
