38200769c44338bb5b8697e5226a6037a1ab34b7
phed <grolgh@gmail.com>
2021-01-28 01:20:42 +0100

    NES Memory Map

--- source/Compiler/systems/systemnes.h
+++ source/Compiler/systems/systemnes.h
@@ -6,19 +6,8 @@
 class SystemNES : public SystemMOS6502
 {
 public:
-    SystemNES(QSharedPointer<CIniFile> settings, QSharedPointer<CIniFile> proj) : SystemMOS6502(settings, proj) {
-        m_processor = MOS6502;
-        m_system = NES;
-        DefaultValues();
-    }
-    void DefaultValues() override {
-        m_startAddress = 0xCA00;
-        m_programStartAddress = 0xCA00;
-        m_ignoreSys = false;
-        m_stripPrg = false;
-
-    }
-
+    SystemNES(QSharedPointer<CIniFile> settings, QSharedPointer<CIniFile> proj);
+    void DefaultValues() override;
 };
 
 #endif // SYSTEMNES_H
