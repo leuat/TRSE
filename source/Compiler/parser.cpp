6621d4ad3747663b09259472e346feed87231e44
Leuat <leuat@irio.co.uk>
2021-01-30 09:21:55 +0100

    Forcing case statements to use variables only

--- source/Compiler/parser.cpp
+++ source/Compiler/parser.cpp
@@ -1752,6 +1752,8 @@ QSharedPointer<Node> Parser::Case()
     QSharedPointer<NodeCase> n = QSharedPointer<NodeCase>(new NodeCase(m_currentToken));
     Eat(); // Eat "case"
     n->m_variable = qSharedPointerDynamicCast<NodeVar>(Variable());
+    if (n->m_variable==nullptr)
+        ErrorHandler::e.Error("Case statements only work with variables.", m_currentToken.m_lineNumber);
     Eat(TokenType::OF);
     while (m_currentToken.m_type != TokenType::END && m_currentToken.m_type != TokenType::ELSE) {
         QSharedPointer<Node> expr = Expr();
