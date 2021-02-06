8c1d6227fea9b729aa32eabb7a241271ded8f012
Leuat <leuat@irio.co.uk>
2021-02-06 16:38:51 +0100

    Finished script

--- Publish/windows_ci/night_build.cmd
+++ Publish/windows_ci/night_build.cmd
@@ -5,20 +5,18 @@ set qt=\Qt\6.0.1\msvc2019_64\bin\
 set uploadScript=C:\Users\leuat\Documents\GitHub\uploadScript\upload_win_night.cmd
 
 set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.16.27023\bin\HostX64\x86
-REM rmdir /s /q %dir%
+rmdir /s /q %dir%
 cd %dir%
-REM git clone https://github.com/leuat/TRSE.git %dir%
+git clone https://github.com/leuat/TRSE.git %dir%
 mkdir Release
 cd Release
 echo "Qmake.."
-REM %qt%\qmake.exe %dir%\trse.pro %dir%\Release
+%qt%\qmake.exe %dir%\trse.pro %dir%\Release
 echo "Compiling.."
-REM \Qt\Tools\QtCreator\bin\jom.exe -f %dir%\Release\Makefile.Release
+\Qt\Tools\QtCreator\bin\jom.exe -f %dir%\Release\Makefile.Release
 cd release
 %qt%\windeployqt.exe .
 
 cd %dir%\Publish\publish_win
 call PublishWindows.cmd
 call %uploadScript%
-
-pause
