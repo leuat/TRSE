914de3e36d3979eb7e9e1d22633a648d1f243796
Leuat <leuat@irio.co.uk>
2021-02-07 13:03:14 +0100

    Updated clear script

--- Publish/tutorials/clear.sh
+++ Publish/tutorials/clear.sh
@@ -6,9 +6,15 @@ find . -name "*.o" -type f -delete
 find . -name "*.dup" -type f -delete
 find . -name "*.nes" -type f -delete
 find . -name "*.com" -type f -delete
-find . -name "*.trse" -type f  -exec sed -i /project_path/d {} +
-find . -name "*.trse" -type f  -exec sed -i /open_files/d {} +
-find . -name "*.trse" -type f  -exec sed -i /current_file/d {} +
+
+#find . -name "*.trse" -type f  -exec sed -i /project_path/d {} +
+#find . -name "*.trse" -type f  -exec sed -i /open_files/d {} +
+#find . -name "*.trse" -type f  -exec sed -i /current_file/d {} +
+
+find . -name "*.trse" -type f  -exec sed -i '' -e /project_path/d {} +
+find . -name "*.trse" -type f  -exec sed -i '' -e /open_files/d {} +
+find . -name "*.trse" -type f  -exec sed -i '' -e /current_file/d {} +
+
 find . -name .backup -exec rm -rf {} \;
 
 echo "open_files =, Hello.ras" >> C64/Tutorials/C64_Tutorials.trse
