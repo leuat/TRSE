c4b7c1dbabbcc9455b53cb0c91bf496649f4b61e
Leuat <leuat@irio.co.uk>
2021-04-14 00:19:52 +0200

    Fixed gameboy tutorials

--- source/Compiler/systems/systemgameboy.cpp
+++ source/Compiler/systems/systemgameboy.cpp
@@ -18,7 +18,7 @@ SystemGameboy::SystemGameboy(QSharedPointer<CIniFile> settings, QSharedPointer<C
 
     m_allowedGlobalTypeFlags << "pure"<<"pure_variable" <<"pure_number" << "signed" <<"no_term" <<"wram" <<"hram" <<"sprram" << "bank";
     m_allowedProcedureTypeFlags << "pure"<<"pure_variable" <<"pure_number" << "signed" <<"no_term" <<"global" << "wram";
-
+    m_renameVariables.clear();
 
 }
 
