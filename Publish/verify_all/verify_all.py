2685c8372875a690b252a769a210c77376d08b8d
Colin Pitrat <colin.pitrat@gmail.com>
2021-04-16 21:49:36 +0100

    Fix usage of subprocess.run

--- Publish/verify_all/verify_all.py
+++ Publish/verify_all/verify_all.py
@@ -145,7 +145,9 @@ def c(path,f1):
 
 
 #	process = Popen([trse, '-cli',  ], stdout=PIPE, stderr=PIPE)
-	return subprocess.run([trse,"-cli",'op=project','project='+projectFile,'input_file='+f1,'assemble='+assemble], stdout=PIPE, stderr=PIPE).returncode
+	result = subprocess.run([trse,"-cli",'op=project','project='+projectFile,'input_file='+f1,'assemble='+assemble], stderr=subprocess.STDOUT)
+	if result.stdout: print(result.stdout)
+	return result.returncode
 
 #	print(rVal)
 #	stdout, stderr = process.communicate()
