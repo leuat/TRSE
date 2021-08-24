63b49457fbe3e3c57e738a111cd1e12d5e2b50db
Leuat <leuat@irio.co.uk>
2021-08-24 17:21:00 +0200

    Forgot to return zero

--- source/Compiler/systems/systemvz200.h
+++ source/Compiler/systems/systemvz200.h
@@ -11,13 +11,8 @@ class SystemVZ200 : public SystemZ80
 {
 public:
     SystemVZ200(QSharedPointer<CIniFile> settings, QSharedPointer<CIniFile> proj);
-
-
-
     virtual void Assemble(QString& text, QString file, QString currentDir, QSharedPointer<SymbolTable>  symTab) override;
     virtual void PostProcess(QString& text, QString file, QString currentDir) override;
-    QString getArkosTrackerSystemString() override {
-    }
 
 };
 
