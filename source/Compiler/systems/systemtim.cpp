603602d810863fb0717a2a46587dcce78db91530
Leuat <leuat@irio.co.uk>
2023-01-10 12:43:46 +0100

    Added TIM tutorials

--- source/Compiler/systems/systemtim.cpp
+++ source/Compiler/systems/systemtim.cpp
@@ -165,7 +165,7 @@ void SystemTIM::ExtraEmulatorCommands()
 void SystemTIM::applyEmulatorParameters(QStringList &params, QString debugFile, QString filename, CIniFile *pini) {
     //    $MAME tim011 -window -v -r 720x512 -switchres -flop1 $FLOPPY.img 1>/dev/null &
 
-    params <<"tim011" <<"-window" <<"-v"<<"-r"<<"720x512"<<"-switchres"<<"-flop1"<<filename+".img";
+    params <<"tim011" <<"-window" <<"-v"<<"-r"<<"720x512"<<"-switchres"<<"-flop1"  <<filename+".img";
 
     m_requireEmulatorWorkingDirectory = true;
 }
