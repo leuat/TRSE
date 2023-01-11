97c30bdcc221fea3ad288d73498c5f12945f3bd2
Leuat <leuat@irio.co.uk>
2023-01-11 22:12:03 +0100

    ADded plasma effect to TIM

--- source/Compiler/systems/systemtim.cpp
+++ source/Compiler/systems/systemtim.cpp
@@ -188,8 +188,8 @@ void SystemTIM::SendKeyCommand(QString keys)
 void SystemTIM::applyEmulatorParameters(QStringList &params, QString debugFile, QString filename, CIniFile *pini) {
     //    $MAME tim011 -window -v -r 720x512 -switchres -flop1 $FLOPPY.img 1>/dev/null &
 
-//    params <<"tim011" <<"-window" <<"-v"<<"-r"<<"720x512"<<"-switchres"<<"-nothrottle" <<"-flop1"  <<filename+".img";
-    params <<"tim011" <<"-window" <<"-v"<<"-r"<<"720x512"<<"-switchres" <<"-flop1"  <<filename+".img";
+    params <<"tim011" <<"-window" <<"-v"<<"-r"<<"720x512"<<"-switchres"<<"-nothrottle" <<"-flop1"  <<filename+".img";
+//    params <<"tim011" <<"-window" <<"-v"<<"-r"<<"720x512"<<"-switchres" <<"-flop1"  <<filename+".img";
 
     m_requireEmulatorWorkingDirectory = true;
 }
