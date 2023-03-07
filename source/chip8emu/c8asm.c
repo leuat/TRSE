2a4dd2dd5f573daf2157c698dcd8925800f6caf3
Leuat <leuat@irio.co.uk>
2023-03-08 00:18:59 +0100

    c8asm bug

--- source/chip8emu/c8asm.c
+++ source/chip8emu/c8asm.c
@@ -155,7 +155,16 @@ static void exit_error(const char *msg, ...) {
 }
 static bool is_arith(char c){
     switch (c){
-        case '0' ... '9':
+        case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
         case '(':
         case '#':
         case '%':
