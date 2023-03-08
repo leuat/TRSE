abdfec5bb405b3b547eb5623f65d8986b24851fc
Leuat <leuat@irio.co.uk>
2023-03-08 13:33:41 +0100

    Fixed up panning, ready for build

--- source/trsedocuments/formimageeditor.cpp
+++ source/trsedocuments/formimageeditor.cpp
@@ -240,20 +240,13 @@ FormImageEditor::~FormImageEditor()
 void FormImageEditor::wheelEvent(QWheelEvent *event)
 {
     float f = event->angleDelta().y()/100.0f;
-    if ((QApplication::keyboardModifiers() & Qt::ShiftModifier)  && (event->button() != Qt::RightButton)) {
+    if ((QApplication::keyboardModifiers() & Qt::ShiftModifier)  && (event->buttons() != Qt::RightButton)) {
         m_updateThread.m_zoom -=f*0.05;
         m_updateThread.m_zoom = std::min(m_updateThread.m_zoom, 1.0f);
         m_updateThread.m_zoom = std::max(m_updateThread.m_zoom, 0.1f);
         float t = 0.2f;
-        QPointF mid = 0.5*(QPointF(m_updateThread.m_work->m_currentImage->m_image->m_width,m_updateThread.m_work->m_currentImage->m_image->m_height));
-//        qDebug() << mid << m_updateThread.m_currentPos;
-//        QPointF pos = -(m_updateThread.m_currentPosInImage-m_updateThread.m_zoomCenter)*m_updateThread.m_zoom + m_updateThread.m_currentPosInImage ;
         QPointF pos = (m_updateThread.m_currentPosInImage);// + m_updateThread.m_zoomCenter ;
-  //      pos = 0.5*(pos + m_updateThread.m_zoomCenter);
-       // pos = m_updateThread.m_zoomCenter*0.5
         m_updateThread.m_zoomCenter = t*pos + (1-t)*m_updateThread.m_zoomCenter;//*(2-2*m_zoom);
-//        QCursor::setPos(m_updateThread.m_zoomCenter.x(),m_updateThread.m_zoomCenter.y());
-      //  m_updateThread.m_zoomCenter = (m_updateThread.m_currentPos);//*(2-2*m_zoom);
         UpdateGrid();
         emit onImageMouseEvent();
         Data::data.forceRedraw = true;
