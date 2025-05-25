c714a3af1b14c4930d3620e2364a5f1feb8c3cad
Zsolt Tarczali <zsolt.tarczali@outlook.com>
2025-05-25 17:04:16 +0200

    just a test

--- mainwindow.h
+++ mainwindow.h
@@ -22,7 +22,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 #include <QMainWindow>
-#include "imageworker.h"
+#include "imageworker.h" 
 #include <QString>
 #include <QPixmap>
 #include <QFontDatabase>
